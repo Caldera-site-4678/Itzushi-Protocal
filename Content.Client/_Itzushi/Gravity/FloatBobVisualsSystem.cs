8d9c3bc5cac9e207e8e94acc59fcc250c5790c36
Finket <viliarttu801@gmail.com>
2026-07-10 14:26:06 +0300

    Port errant souls

---
+++ Content.Client/_Itzushi/Gravity/FloatBobVisualsSystem.cs
@@ -0,0 +1,37 @@
+using Content.Shared._Itzushi.Gravity;
+using Robust.Client.GameObjects;
+using Robust.Shared.Timing;
+using System.Numerics;
+
+namespace Content.Client._Itzushi.Gravity;
+
+/// <summary>
+/// handles visual bobbing for floating entities
+/// this only changes the sprite offset on the client ONLY
+/// </summary>
+
+public sealed partial class FloatBobVisualsSystem : EntitySystem
+{
+    [Dependency] private IGameTiming _timing = default!;
+    [Dependency] private SpriteSystem _sprite = default!;
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var time = (float) _timing.CurTime.TotalSeconds;
+        var query = EntityQueryEnumerator<FloatBobVisualsComponent, SpriteComponent>();
+
+        while (query.MoveNext(out var uid, out var bob, out var sprite))
+        {
+            if (!bob.BaseOffsetSet)
+            {
+                bob.BaseOffset = sprite.Offset;
+                bob.BaseOffsetSet = true;
+            }
+
+            var yOffset = MathF.Sin(time * MathF.Tau * bob.Frequency) * bob.Amplitude;
+            _sprite.SetOffset(uid, bob.BaseOffset + new Vector2(0f, yOffset));
+        }
+    }
+}
